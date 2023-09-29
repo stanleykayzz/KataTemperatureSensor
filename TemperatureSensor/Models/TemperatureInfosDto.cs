a9450d7bbefaacafbd828312654904da0df7e07a
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-09-30 00:02:47 +0200

    Adding temporary dataset and temperature object

---
+++ TemperatureSensor/Models/TemperatureInfosDto.cs
@@ -0,0 +1,24 @@
+﻿namespace TemperatureSensor.Models
+{
+    public class TemperatureInfosDto
+    {
+        public int Id { get; set; }
+        public int Temperature { get; set; }
+        public string State { get; set; }
+        public DateTime Date { get; set; }
+
+        public TemperatureInfosDto()
+        {
+            
+        }
+
+        public TemperatureInfosDto(int id, TempereatureInfo tempereatureInfo)
+        {
+            Id = id;
+            Temperature = tempereatureInfo.Temperature;
+            State = tempereatureInfo.State;
+            Date = (DateTime)tempereatureInfo.Date;
+        }
+
+    }
+}
---
+++ TemperatureSensor/Models/TempereatureInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace TemperatureSensor.Models
+{
+    public class TempereatureInfo
+    {
+        public int Temperature { get; set; }
+        public string? State { get; set; }
+        public DateTime? Date { get; set; }
+
+        public TempereatureInfo(int temperature)
+        {
+            if (temperature < 19) State = "COLD";
+            else if (temperature >= 19 && temperature < 30) State = "WARM";
+            else if (temperature >= 30) State = "HOT";
+            Date = DateTime.Now;
+            Temperature = temperature;
+        }
+        public TempereatureInfo(int temperature, string etat, DateTime date)
+        {
+            Temperature = temperature;
+            State = etat;
+            Date = date;
+        }
+    }
+}
