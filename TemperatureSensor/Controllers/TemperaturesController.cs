c51b0f97dae41db685961a5331d4b5cb21daaa56
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-09-29 21:24:14 +0200

    api to get all temperatures

---
+++ TemperatureSensor/Controllers/TemperaturesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureSensor.Controllers
+{
+    [ApiController]
+    [Route("api/temperatures")]
+    public class TemperaturesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetTemperatures()
+        {
+            return new JsonResult(TemperatureInfosDataStore.Current.TemperatureInfos);
+        }
+    }
+}
