230e6df6256cefde871c0b73a67c3c196343370a
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-09-29 23:58:15 +0200

    adding methods in controller to add temperature, get all temperature and get last x temperatures

--- TemperatureSensor/Controllers/TemperaturesController.cs
+++ TemperatureSensor/Controllers/TemperaturesController.cs
@@ -1,15 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
+using TemperatureSensor.Models;
 
 namespace TemperatureSensor.Controllers
 {
     [ApiController]
-    [Route("api/temperatures")]
+    [Route("api/")]
     public class TemperaturesController : ControllerBase
     {
-        [HttpGet]
-        public JsonResult GetTemperatures()
+        [HttpGet("temperatures")]
+        public ActionResult<TemperatureInfosDto> GetTemperatures()
         {
-            return new JsonResult(TemperatureInfosDataStore.Current.TemperatureInfos);
+            return Ok(TemperatureInfosDataStore.Current.TemperatureInfos);
+        }
+
+        [HttpGet("temperatures/last/{number}")]
+        public ActionResult<TemperatureInfosDto> GetLastTemperaturesByNumber(int number)
+        {
+            var count = TemperatureInfosDataStore.Current.TemperatureInfos.Count();
+            var lastXList = TemperatureInfosDataStore.Current.TemperatureInfos.Skip(Math.Max(0, count - number));
+
+            if(count < number)
+            {
+                return NotFound();
+            }
+            return Ok(lastXList);
+        }
+
+        [HttpGet("temperature/{id}")]
+        public ActionResult<TemperatureInfosDto> GetTemperature(int id)
+        {
+            var temperatureInfo = TemperatureInfosDataStore.Current.TemperatureInfos.FirstOrDefault(x => x.Id == id);
+            
+            if (temperatureInfo == null) { return NotFound(); }
+
+            return Ok(temperatureInfo);
+        }
+            
+        [HttpPost("captor/{temperature}")]
+        public ActionResult PostTemperature(int temperature)
+        {
+            var countTemperatures = TemperatureInfosDataStore.Current.TemperatureInfos.Count();
+            var Temperature = new TempereatureInfo(temperature);
+            var PostedTemperature = new TemperatureInfosDto(countTemperatures + 1, Temperature);
+            TemperatureInfosDataStore.Current.TemperatureInfos.Add(
+                PostedTemperature
+            );
+            return Ok(PostedTemperature);
         }
     }
 }
