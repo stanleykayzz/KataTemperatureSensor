c73b260679de3415b4f0aeeb35bcad124f9778e3
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-10-05 14:40:41 +0200

    Updating temperature controllers

--- TemperatureSensor/Controllers/TemperaturesController.cs
+++ TemperatureSensor/Controllers/TemperaturesController.cs
@@ -9,26 +9,26 @@ namespace TemperatureSensor.Controllers
     public class TemperaturesController : ControllerBase
     {
         [HttpGet("temperatures")]
-        public ActionResult<TemperatureInfosDto> GetTemperatures()
+        public ActionResult<TemperatureInfoDto> GetTemperatures()
         {
             return Ok(TemperatureInfosDataStore.Current.TemperatureInfos);
         }
 
         [HttpGet("temperatures/last/{number}")]
-        public ActionResult<TemperatureInfosDto> GetLastTemperaturesByNumber(int number)
+        public ActionResult<TemperatureInfoDto> GetLastTemperaturesByNumber(int number)
         {
             var count = TemperatureInfosDataStore.Current.TemperatureInfos.Count();
-            var lastXList = TemperatureInfosDataStore.Current.TemperatureInfos.Skip(Math.Max(0, count - number));
+            var LastXTemperatures = TemperatureInfosDataStore.Current.TemperatureInfos.Skip(Math.Max(0, count - number));
 
             if(count < number)
             {
                 return NotFound();
             }
-            return Ok(lastXList);
+            return Ok(LastXTemperatures);
         }
 
         [HttpGet("temperature/{id}")]
-        public ActionResult<TemperatureInfosDto> GetTemperature(int id)
+        public ActionResult<TemperatureInfoDto> GetTemperature(int id)
         {
             var temperatureInfo = TemperatureInfosDataStore.Current.TemperatureInfos.FirstOrDefault(x => x.Id == id);
             
@@ -41,12 +41,44 @@ namespace TemperatureSensor.Controllers
         public ActionResult PostTemperature(int temperature)
         {
             var countTemperatures = TemperatureInfosDataStore.Current.TemperatureInfos.Count();
-            var Temperature = new TempereatureInfo(temperature);
-            var PostedTemperature = new TemperatureInfosDto(countTemperatures + 1, Temperature);
+            var PostedTemperature = new TemperatureInfoDto(countTemperatures + 1, temperature);
             TemperatureInfosDataStore.Current.TemperatureInfos.Add(
                 PostedTemperature
             );
             return Ok(PostedTemperature);
+        }
+
+        [HttpGet("temperature/limits")]
+        public ActionResult<StateLimitDto> GetTemperatureLimits()
+        {
+            return Ok(StateLimitDataStore.CurrentLimits.StateLimits);
+        }
+
+        [HttpPost]
+        [Route("temperature/limit")]
+        public ActionResult SetTemperatureStateLimit(
+            [FromQuery] int hot,
+            [FromQuery] int cold,
+            [FromQuery] int warm
+            )
+        {
+            var StateLimits = StateLimitDataStore.CurrentLimits.StateLimits;
+            if (hot != null)
+            {
+                var hotItem = StateLimits.FirstOrDefault(x => x.Name == "HOT");
+                hotItem.Value = hot;
+            }
+            if (cold != null)
+            {
+                var coldItem = StateLimits.FirstOrDefault(x => x.Name == "COLD");
+                coldItem.Value = cold;
+            }
+            if (warm != null)
+            {
+                var warmItem = StateLimits.FirstOrDefault(x => x.Name == "WARM");
+                warmItem.Value = warm;
+            }
+            return Ok("Updated Temperature state limit");
         }
     }
 }
