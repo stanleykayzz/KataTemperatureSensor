3bf594403ea0cdaa0a579a79ebe3cf9e0fee39ef
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-10-06 15:07:19 +0200

    Adding NUnit for Test project

---
+++ TemperatureSensorTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TemperatureSensorTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
