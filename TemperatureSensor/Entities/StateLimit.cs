679106f8144b6bc38300af49d536192247bec4e5
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-10-06 15:54:56 +0200

    Adding Test for controller

--- TemperatureSensor/Entities/StateLimit.cs
+++ TemperatureSensor/Entities/StateLimit.cs
@@ -10,5 +10,16 @@ namespace TemperatureSensor.Entities
         public int StateLimitsId { get; set; }
         public string Name { get; set; }
         public int Value { get; set; }
+
+        public StateLimit()
+        {
+            
+        }
+
+        public StateLimit(string name, int limit)
+        {
+            Name = name;
+            Value = limit;
+        }
     }
 }
---
+++ TemperatureSensorTests/Controllers/TemperaturesControllerUT.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureSensorTests.Controllers
+{
+    [TestFixture]
+    public class TemperaturesControllerUT
+    {
+        private HttpClient _httpClient;
+        private WebApplicationFactory<Program> _webApplicationFactory;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _webApplicationFactory = new WebApplicationFactory<Program>();
+            _httpClient = _webApplicationFactory.CreateClient();
+        }
+        [OneTimeTearDown]
+        public void OnTimeTearDow()
+        {
+            _httpClient.Dispose();
+            _webApplicationFactory?.Dispose();
+        }
+
+        [Test]
+        public async Task GetStateLimits_ReturnOKResult()
+        {
+
+            // Arrange - No need to arrange anything for this test
+
+            // Act
+            var response = await _httpClient.GetAsync("/api/temperatures");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
