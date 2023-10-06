f07078521c8d5720b4807c701a9fdacdc1c8fb6e
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-10-06 15:32:08 +0200

    Adding Test for entities

--- TemperatureSensorTests/Entities/StateLimitUt.cs
+++ TemperatureSensorTests/Entities/StateLimitUt.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TemperatureSensor.Entities;
 
 namespace TemperatureSensorTests.Entities
 {
+    [TestFixture]
     public class StateLimitUt
     {
+        [Test]
+        [TestCase("Hot", 33)]
+        [TestCase("Cold", 16)]
+        [TestCase("Warm", 20)]
+        public void StateLimit_PropertiesOk_WithValues(string name, int limit)
+        {
+            var stateLimit = new StateLimit(name,limit);
+
+            // Act & Assert
+            Assert.AreEqual(name, stateLimit.Name);
+            Assert.AreEqual(limit, stateLimit.Value);
+        }
     }
 }
---
+++ TemperatureSensorTests/Entities/TemperatureInfoUT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureSensor.Entities;
+
+namespace TemperatureSensorTests.Entities
+{
+    [TestFixture]
+    public class TemperatureInfoUT
+    {
+        [Test]
+        [TestCase(16)]
+        [TestCase(22)]
+        public void Test_TemperatureInfoOKWitParams(int temperature)
+        {
+            var temperatureInfo = new TemperatureInfo(temperature);
+
+            Assert.IsNotNull(temperatureInfo);
+            Assert.IsNotNull(temperatureInfo.State);
+            Assert.AreEqual(temperatureInfo.Temperature, temperature);
+        }
+    }
+}
