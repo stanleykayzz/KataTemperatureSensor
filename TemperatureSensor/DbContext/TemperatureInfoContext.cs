34c8e485602d68c7d4470553c3f1457369a4fd3e
alvin_ondzounga <alvin.ondzounga@channeladvisor.com>
2023-10-10 00:54:20 +0200

    updating data context

--- TemperatureSensor/DbContext/TemperatureInfoContext.cs
+++ TemperatureSensor/DbContext/TemperatureInfoContext.cs
@@ -12,7 +12,6 @@ namespace TemperatureSensor
         public TemperatureInfoContext(DbContextOptions<TemperatureInfoContext> options)
             : base(options)
         {
-            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -47,56 +46,56 @@ namespace TemperatureSensor
                     State = "COLD",
                     Date = DateTime.Now,                  
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 2,
                     Temperature = 24,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 3,
                     Temperature = 30,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 4,
                     Temperature = 26,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 5,
                     Temperature = 34,
                     State = "HOT",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 6,
                     Temperature = 24,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 7,
                     Temperature = 30,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 8,
                     Temperature = 26,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 9,
                     Temperature = 34,
@@ -104,7 +103,7 @@ namespace TemperatureSensor
                     Date = DateTime.Now,
                 },
 
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 10,
                     Temperature = 8,
@@ -112,35 +111,35 @@ namespace TemperatureSensor
                     Date = DateTime.Now,
                 },
 
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 11,
                     Temperature = 18,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 12,
                     Temperature = 24,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 13,
                     Temperature = 30,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 14,
                     Temperature = 26,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 15,
                     Temperature = 34,
@@ -148,98 +147,98 @@ namespace TemperatureSensor
                     Date = DateTime.Now,
                 },
 
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 16,
                     Temperature = 18,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 17,
                     Temperature = 2,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 18,
                     Temperature = 6,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 19,
                     Temperature = 40,
                     State = "HOT",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 20,
                     Temperature = 25,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 21,
                     Temperature = 18,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 22,
                     Temperature = 47,
                     State = "HOT",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 23,
                     Temperature = 38,
                     State = "HOT",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 24,
                     Temperature = 14,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 25,
                     Temperature = 15,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 26,
                     Temperature = 36,
                     State = "HOT",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 27,
                     Temperature = 18,
                     State = "COLD",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 28,
                     Temperature = 27,
                     State = "WARM",
                     Date = DateTime.Now,
                 },
-                new TemperatureInfoDto()
+                new TemperatureInfo()
                 {
                     Id = 29,
                     Temperature = 50,
